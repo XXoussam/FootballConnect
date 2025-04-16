3cd90cd9b15ebf7184a5f2533aab1f60ad716b75
saoudi oussama <35174306-saoudioussama13@users.noreply.replit.com>
2025-04-16 07:53:38 +0000

    first commit

--- client/src/components/network/SuggestedConnections.tsx
+++ client/src/components/network/SuggestedConnections.tsx
@@ -56,7 +56,7 @@ const SuggestedConnections = ({ limit = 2 }: SuggestedConnectionsProps) => {
         fullName: "Jürgen Klopp",
         position: "Manager",
         club: "Liverpool FC",
-        avatarUrl: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=100&q=80"
+        avatarUrl: ""
       }
     }
   ];
